fd64a462f2f2dde74a593d9efbfab68882ccd74c
agsilvamhm <adalberto.goncalves.silva@gmail.com>
2023-05-04 21:19:57 -0300

    Registrando o início do segundo módulo

--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,9 +2,13 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+    //private negociacoes: Negociacao[]=[];
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
+    // lista(): ReadonLy Negociacao[] { 
+    //  return this.negociacoes;
+    //    }
     lista() {
         return this.negociacoes;
     }
